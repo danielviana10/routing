ae7d6c643286415181bccf5449d16d746876f8a3
danielviana10 <danielvianna43@gmail.com>
2025-02-12 14:44:57 -0300

    feat: params and queryParams

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstPageComponent } from './first-page/first-page.component';
 import { SecondPageComponent } from './second-page/second-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageWithParamsComponent } from './page-with-params/page-with-params.component';
 
 const routes: Routes = [
   {path: 'first-page', component: FirstPageComponent},
   {path: 'second-page', component: SecondPageComponent},
   {path: '', redirectTo: '/first-page', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent}
+  {path: 'page-with-params/:id', component: PageWithParamsComponent},
+  {path: '**', component: PageNotFoundComponent},
 ]
 
 
